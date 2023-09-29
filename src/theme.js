43da9a5a924e7b825eeb3497f419b068f194c383
HARDEEP MOHANTY <126688952+hardeepmty@users.noreply.github.com>
2023-09-29 21:19:51 +0530

    Add files via upload

---
+++ src/theme.js
@@ -0,0 +1,17 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette:{
+    primary:{
+      main:"#333" ,
+      light:"skyblue"
+    },
+    secondary:{
+      main:"#456" ,
+    },
+    otherColor:{
+      main:"#999"
+    }
+  }
+})
+
